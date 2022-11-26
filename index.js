37a6262d42c2a0ab9fe215bd5eaab639dad73089
roza <rozanawzad2001@gmail.com>
2022-11-26 19:20:44 +0300

    done js 2 array lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+
+const cats=["Milo", "Otis", "Garfield"];
+  function destructivelyAppendCat(name){
+    cats.push("Ralph")
+  }
+  function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+  }
+  function destructivelyRemoveLastCat(){
+    cats.pop()
+  }
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+function appendCat(name){
+ const cat= [...cats, "Broom"];
+ return cat
+}
+function  prependCat(name){
+  const cat=["Arnold", ...cats]
+  return cat
+}
+function removeLastCat(){
+  const cat=[...cats]
+  cat.pop()
+  return cat
+}
+function  removeFirstCat(){
+  const cat=[...cats]
+  cat.shift()
+  return cat
+}
